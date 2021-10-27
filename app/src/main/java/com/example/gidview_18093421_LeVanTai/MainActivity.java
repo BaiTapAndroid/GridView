b655671184649b38b5d410879665b0d10dc366ce
leanhthanhtai <leanhthanhtai@gmail.com>
2021-10-27 09:37:19 +0700

    GridView

---
+++ app/src/main/java/com/example/gidview_18093421_LeVanTai/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.gidview_18093421_LeVanTai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gvThing;
+    ArrayList<ElectricThing> arrayList;
+    CustomAdapter adt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gvThing=findViewById(R.id.gvThing);
+        arrayList =new ArrayList<>();
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        arrayList.add(new ElectricThing("day nguon",3,15000,R.drawable.daynguon_1));
+        adt = new CustomAdapter(MainActivity.this,R.layout.item_gridview,arrayList);
+        gvThing.setAdapter(adt);
+    }
+}
